e0d6fec22130442b97b683e8ea3f3b0886691ee9
vheekey <vicformidable@gmail.com>
2025-01-14 10:54:04 +0100

    Publishes message to kafka on book insertion

--- src/main/java/com/vheekey/book_service/services/BookServiceImpl.java
+++ src/main/java/com/vheekey/book_service/services/BookServiceImpl.java
@@ -6,23 +6,31 @@ import com.vheekey.book_service.repositories.BookRepository;
 import com.vheekey.book_service.requests.BookRequest;
 import com.vheekey.book_service.responses.BookResponse;
 import com.vheekey.book_service.services.interfaces.BookService;
+import com.vheekey.book_service.services.interfaces.KafkaService;
 import org.springframework.stereotype.Service;
 
-@Service()
+@Service
 public class BookServiceImpl implements BookService {
 
     private final BookMapper bookMapper;
     private final BookRepository bookRepository;
+    private final KafkaService kafkaService;
 
-    public BookServiceImpl(BookMapper bookMapper, BookRepository bookRepository) {
+    public BookServiceImpl(
+            BookMapper bookMapper,
+            BookRepository bookRepository,
+            KafkaService kafkaService
+    ) {
         this.bookMapper = bookMapper;
         this.bookRepository = bookRepository;
+        this.kafkaService = kafkaService;
     }
 
     @Override
     public BookResponse addBook(BookRequest request) {
         Book book = this.bookMapper.toEntity(request);
         book = this.bookRepository.save(book);
+        this.kafkaService.send(book);
 
         return this.bookMapper.toResponse(book);
     }
